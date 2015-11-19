bfd09f3aeebebcb2e85390c85474322451762929
Chris <rannchris@gmail.com>
2015-11-19 09:45:49 -0800

    hopefully final changes

---
+++ danbiY.h
@@ -0,0 +1,18 @@
+#ifndef _DANBIY_H_
+#define _DANBIY_H_
+
+
+extern void menu(void);
+extern void menuRender(void);
+extern void initButton(void);
+extern void checkMouse(XEvent *);
+extern void initGameOver(void);
+extern void gameOverRender(void);
+extern void initImageToLeft();
+extern void initImageToRight();
+extern void stayAnime(bool);
+extern void walkAnime(bool);
+extern void jumpAnime(bool);
+extern void shootAnime(bool, char);
+
+#endif
